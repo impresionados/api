dbaa370a36766ad952b9c0b850d1e3d00e6a9c72
Zino243 <67828167+Zino243@users.noreply.github.com>
2025-01-31 10:06:35 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+# Definir el puerto con un valor por defecto si no está en las variables de entorno
+port = os.getenv("PORT", "8000")
+
+# Comando para ejecutar uvicorn
+cmd = ["uvicorn", "api2.app:app", "--host", "0.0.0.0", "--port", port, "--reload"]
+
+# Ejecutar el comando
+subprocess.run(cmd)
